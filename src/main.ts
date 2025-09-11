e969c4fc9d109bd6df0284328dde4c32a68437e3
Jerry-Khobby <jerrymardeburg@gmail.com>
2025-09-11 10:10:35 -1200

    trying to add swagger UI docs to the backend

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule,DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //create swagger config 
+  const config = new DocumentBuilder()
+  .setTitle('Hospital Management API')
+  .setDescription('The Hospital Management API description')
+  .addTag('hospital-management')
+  .setVersion('1.0')
+  .build();
+  const document =()=> SwaggerModule.createDocument(app,config);
+  SwaggerModule.setup('api',app,document);
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
